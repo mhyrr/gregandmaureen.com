03cad1919d93b71627c31dda025d02ed09282d95
mhyrr <grolsen@gmail.com>
2010-03-04 01:42:05 -0500

    First cut for files, simplest possible bad jquery, basic layout and images.

---
+++ _site/js/fade.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+
+	$("#details a").click(function() {
+		//alert("handler for click called");
+		$("#overlay").fadeOut(250);
+		$("#details_content").fadeIn(250);
+	
+	});
+	
+});
